37083062135879e5c4563bd7ec2e645f903342e6
pieraqua <pauloffxii@hotmail.com>
2022-06-02 23:35:43 -0300

    teste

--- LAB03/LAB03/teste.c
+++ LAB03/LAB03/teste.c
@@ -11,20 +11,28 @@ void teste()
 {
     lcdOn_Off();
 
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_A);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_B);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_C);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_D);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_E);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_F);
-    while(LCD_checkBusyFlag()){}
-    displayChar(SIMB_G);
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_A);
+    }
+
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_B);
+    }
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_C);
+    }
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_D);
+    }
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_E);
+    }
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_F);
+    }
+    while(LCD_checkBusyFlag()){
+        displayChar(SIMB_G);
+    }
 
     delay(80000000);
 
